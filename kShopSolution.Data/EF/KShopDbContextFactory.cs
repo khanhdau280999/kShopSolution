0efd3889c994933d6ac33f2794e455dc5497cb7f
KhanhPhuQuoc <phuquockhanh10d5@gmail.com>
2020-12-01 16:57:20 +0700

    design database config migration database

---
+++ kShopSolution.Data/EF/KShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace kShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<KshopDbContext>
+    {
+        public KshopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("kShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<KshopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new KshopDbContext(optionsBuilder.Options);
+        }
+    }
+}
