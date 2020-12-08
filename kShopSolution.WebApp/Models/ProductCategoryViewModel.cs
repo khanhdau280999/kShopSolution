d1d85a74a3cb3cff2245ee2bd2c899d99cbefba4
KhanhPhuQuoc <phuquockhanh10d5@gmail.com>
2020-12-08 10:41:00 +0700

    web_app

---
+++ kShopSolution.WebApp/Models/ProductCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using kShopSolution.ViewModels.Catalog.Categories;
+using kShopSolution.ViewModels.Catalog.Products;
+using kShopSolution.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kShopSolution.WebApp.Models
+{
+    public class ProductCategoryViewModel
+    {
+        public CategoryVm Category { get; set; }
+
+        public PagedResult<ProductVm> Products { get; set; }
+    }
+}
